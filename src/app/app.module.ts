46dad0863895f42eefe572eab882821e5a00b919
jbrownssf <jpbrown@softstackfactory.org>
2017-05-04 04:42:50 +0000

    Landing, Login, Register, Home  Pages

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { Landing } from '../pages/landing/landing';
+import { Login } from '../pages/login/login';
+import { Register } from '../pages/register/register';
+import { Home } from '../pages/home/home';
+
+let injections: any[] = [
+    MyApp,
+    Landing,
+    Login,
+    Register,
+    Home
+  ]
+
+@NgModule({
+  declarations: injections,
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: injections,
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
